a0e36bde3a887de7fb84fda4e0b2fa5636dc901e
Picasso Houessou <houessoupicasso@yahoo.fr>
2024-03-10 14:26:35 +0100

    Add rector php

---
+++ rector.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+use Rector\Symfony\Set\SensiolabsSetList;
+use Rector\Symfony\Set\SymfonySetList;
+use Rector\Doctrine\Set\DoctrineSetList;
+use Rector\Set\ValueObject\LevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->symfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml');
+
+    $rectorConfig->sets([
+        // LevelSetList::UP_TO_PHP_81,
+         DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
+         SymfonySetList::ANNOTATIONS_TO_ATTRIBUTES,
+        SymfonySetList::SYMFONY_64,
+        SymfonySetList::SYMFONY_CODE_QUALITY,
+    ]);
+};
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -113,7 +113,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     }
 
 
-    public function getRoles()
+    public function getRoles(): array
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
